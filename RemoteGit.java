07c415d3cf4b23b4f65ad894a7a1d48fe3eee8eb
Blake Feehan <feehanb@ferris.edu>
2026-06-18 00:11:48 -0400

    Add feature structure

--- RemoteGit.java
+++ RemoteGit.java
@@ -4,54 +4,65 @@ import java.util.Scanner;
 
 public class RemoteGit {
 
-			public static void main(String args[]) {
+	public static void main(String args[]) {
 
-				Scanner scnr = new Scanner(System.in);
+		Scanner scnr = new Scanner(System.in);
 
-				String confirmExamInput;
-				
-				double gradeNum;
-				
-				char confirmToUpper;
-				char gradeLttr = 'F'; // Had to initialize here was throwing errors.
+		String confirmExamInput;
 
-				// Greet user
-				System.out.println("Hello! Welcome to the letter grade assignment task!");
-				System.out.println("---------------------------------------------------\n");
+		double gradeNum;
 
-				// Request user input
-				System.out.println("Did you attend the final exam? Yes or No?");
-				confirmExamInput = scnr.nextLine();
-				confirmToUpper = Character.toUpperCase(confirmExamInput.charAt(0));
+		char confirmToUpper;
+		char gradeLttr = 'F'; // Had to initialize here was throwing errors.
 
-				if (confirmToUpper == 'Y') {
-					System.out.println("Great! Please enter your numerical grade (0-100).");
-					gradeNum = scnr.nextDouble();
+		// Greet user
+		System.out.println("Hello! Welcome to the letter grade assignment task!");
+		System.out.println("---------------------------------------------------\n");
 
-					if (gradeNum >= 90) {
-						gradeLttr = 'A';
-					} else if (gradeNum >= 80) {
-						gradeLttr = 'B';
-					} else if (gradeNum >= 70) {
-						gradeLttr = 'C';
-					} else if (gradeNum >= 60) {
-						gradeLttr = 'D';
-					} else {
-						gradeLttr = 'F';
-					}
+		// Request user input
+		System.out.println("Did you attend the final exam? Yes or No?");
+		confirmExamInput = scnr.nextLine();
+		confirmToUpper = Character.toUpperCase(confirmExamInput.charAt(0));
 
-				}
+		if (confirmToUpper == 'Y') {
+			System.out.println("Great! Please enter your numerical grade (0-100).");
+			gradeNum = scnr.nextDouble();
 
-				else if (confirmToUpper == 'N') {
-					gradeLttr = 'F';
-				}
-
-				System.out.println("Your corrisponding letter grade for this exam is: " + gradeLttr);
-
-				scnr.close();
-
+			if (gradeNum >= 90) {
+				gradeLttr = 'A';
+			} else if (gradeNum >= 80) {
+				gradeLttr = 'B';
+			} else if (gradeNum >= 70) {
+				gradeLttr = 'C';
+			} else if (gradeNum >= 60) {
+				gradeLttr = 'D';
+			} else {
+				gradeLttr = 'F';
 			}
+
 		}
-	
+
+		else if (confirmToUpper == 'N') {
+			gradeLttr = 'F';
+		}
+
+		System.out.println("Your corrisponding letter grade for this exam is: " + gradeLttr);
+
+		if (gradeLttr == 'A') {
+			System.out.println("Wow good job!");
+		} else if (gradeLttr == 'B') {
+			System.out.println("Good job.");
+
+		} else if (gradeLttr == 'C') {
+			System.out.println("Atleast you had passed!");
+
+		} else {
+			System.out.println("You Failed!");
+		}
+
+		scnr.close();
+
+	}
+}
 
 
